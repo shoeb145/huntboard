0ecfadd9aca6ad36b972385896335126f413346f
shoeb145 <cshoeb07@gmail.com>
2025-09-09 18:27:18 +0530

    Update frontend configs, dependencies, and Tailwind setup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,10 +4,12 @@ import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["Edu AU VIC WA NT Arrows", "cursive"],
+    },
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ["synthwave", "forest"],
+    themes: ["black", "light"],
   },
 };
